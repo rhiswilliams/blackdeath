c0efc44b8bee0848bef4ee710358d241f1383f51
rhiswilliams <55388591+rhiswilliams@users.noreply.github.com>
2019-11-11 09:54:48 +0000

    Add files via upload

---
+++ blackdeath.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Import libraries
+import csv
+import matplotlib
+import matplotlib.pyplot
+matplotlib.use('TkAgg')
+from matplotlib import colors
+import tkinter
+from tkinter import simpledialog
+
+# Create lists
+population = []
+poprow = []
+ratscaught = []
+ratsrow = []
+deaths=[]
+deathrow=[]
+deathstotal=[]
+
+### Reading in data ###
+
+# Read in population data to population list
+f = open('death.parishes.txt', newline='')
+
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+for row in reader:    
+    population.append(row)
+    poprow = []
+    for people in row:
+        poprow.append(people)
+    
+f.close()
+
+"""
+# Test data import by finding total
+poptotal=[]
+for poprow in population:
+    poptotal.append((sum(poprow)))
+
+print(sum(poptotal))
+"""
+
+# Read in data of average rats caught to ratscaught list
+f = open('death.rats.txt', newline='')
+
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+for value in reader:
+    ratsrow=[]
+    ratscaught.append(value)
+    for rat in value:
+        ratsrow.append(rat)
+    
+f.close()
+
+"""
+# Test data import by finding total
+rattotal=[]
+for ratsrow in ratscaught:
+    rattotal.append((sum(ratsrow)))
+print(sum(rattotal))
+"""
+
+
+### Defining functions ###
+
+
+# Define function to create map
+# Set map area
+# Remove tick labels
+# Set colour bar
+# Set title name
+# Display map
+
+def create_map(datalist, titlename):
+    fig,ax = matplotlib.pyplot.subplots()
+    cmap = colors.ListedColormap(['#FFFFFF', '#E2E2E2', '#C6C6C6', '#AAAAAA',
+                                  '#8D8D8D', '#717171', '#555555', '#383838',
+                                  '#1C1C1C', '#000000'])
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    matplotlib.pyplot.title(titlename)
+    matplotlib.pyplot.imshow(datalist, cmap=cmap)
+    matplotlib.pyplot.colorbar()
+
+
+# Create map using population data
+def runpop():
+    create_map(population, 'Population density')
+
+
+# Create map using rats caught data
+def runrat():
+    create_map(ratscaught, 'Numbers of rats caught')     
+
+
+# Get user input for equation weighting
+# Calculate deaths data
+# Create map using deaths data
+    
+def rundeath():
+    rat_inp = simpledialog.askfloat(title="Input",
+                                  prompt="What is the rat weighting?:",
+                                  initialvalue=0.8)
+
+
+    pop_inp = simpledialog.askfloat(title="Input",
+                                  prompt="What is the population weighting?:",
+                                  initialvalue=1.3)
+
+    calc_deaths(rat_inp,pop_inp)
+    create_map(deaths, 'Estimated deaths')
+    
+    
+# Get user input for equation weighting
+# Calculate deaths data
+# Save deaths data to directory as txt
+# Display message to tell user data is saved
+    
+def maketxt():
+    rat_inp = simpledialog.askfloat(title="Input",
+                                  prompt="What is the rat weighting?:",
+                                  initialvalue=0.8)
+
+
+    pop_inp = simpledialog.askfloat(title="Input",
+                                  prompt="What is the population weighting?:",
+                                  initialvalue=1.3)
+
+    calc_deaths(rat_inp,pop_inp)
+    f = open('death.deaths.txt','w', newline='\n')
+    newfile = csv.writer(f)
+    for deathrow in deaths:
+        newfile.writerow(deathrow)
+    tkinter.messagebox.showinfo("File saved", ('Deaths data saved to current directory'))
+    f.close()
+
+
+# Close window to exit
+def close_window(): 
+    root.destroy()
+
+
+# Get user input for equation weighting
+# Calculate deaths data
+# Sum deaths data to find total number of deaths
+# Display total in message    
+    
+def total_deaths():    
+    rat_inp = simpledialog.askfloat(title="Input",
+                                  prompt="What is the rat weighting?:",
+                                  initialvalue=0.8)
+
+
+    pop_inp = simpledialog.askfloat(title="Input",
+                                  prompt="What is the population weighting?:",
+                                  initialvalue=1.3)
+
+    calc_deaths(rat_inp,pop_inp)
+    
+    for deathrow in deaths:
+        deathstotal=[]
+        deathstotal.append((sum(deathrow)))
+    deathsum = sum(deathstotal)
+    tkinter.messagebox.showinfo("Total deaths", ('The estimated total number of deaths is ' + str(deathsum))) 
+
+
+# Simultaneously read population and rat caughts data to find data pairs
+# Caluclate death figure using data pairs and weighting from input
+    
+def calc_deaths(rat_inp,pop_inp):    
+    for poprow, ratsrow in zip(population, ratscaught):
+        deathrow=[]
+        for p, r in zip(poprow, ratsrow):
+            d = (rat_inp*r)*(pop_inp*p)
+            deathrow.append(d)
+        deaths.append(deathrow)
+    """
+    # Test contents of deaths list
+    print(deaths)
+    """
+    
+### Create GUI ###
+
+fig, ax = matplotlib.pyplot.subplots()
+
+# Set opening canvas with introductary text
+root = tkinter.Tk()
+root.geometry('400x400')
+c = tkinter.Canvas(root, height=400, width =400, bg="white")
+c.create_text(200,40,text='The Black Death hit London in 1665.\nSelect the options on the menu bar to find out more.')
+c.pack()
+
+
+#  Create model canvas
+root.wm_title("Model")
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+
+# Create menu and commands to run functions
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label="Model", menu=model_menu)
+model_menu.add_command(label="Population graph", command=runpop)
+model_menu.add_command(label="Rat population graph", command=runrat)
+model_menu.add_command(label="Estimated deaths graph", command=rundeath)
+model_menu.add_command(label="Show total deaths", command=total_deaths)
+model_menu.add_command(label="Save deaths data as txt", command=maketxt)
+model_menu.add_command(label="Exit", command=close_window)
+
+tkinter.mainloop()
---
+++ readme.txt
@@ -0,0 +1,55 @@
+CONTENTS LIST
+
+- blackdeath.ipynb : Jupyter notebook version
+- blackdeath.py : alternative Py version
+- death.parishes : human population data
+- death.rats : rat population data
+- Previous Versions folder : 5 older versions of the project throughout its development
+
+
+WHAT IS THE PROGRAM?
+
+The program reads in data of rat populations recorded by rat catchers and population density derived from parish 
+records. The data is recorded according to 100m x 100m squares of London. The program displays this data as
+raster maps.
+
+Using the equation d = (0.8 x r) x (1.3 x p), the program calculates the estimated number of deaths for each
+square. The program displays this data as a raster map, displays the total sum of the estimated number of
+deaths, and saves the data as a txt file in the current working directory. The 0.8 and 1.3 weightings can be
+changed to different values through user input.
+
+Potential uses for the program include:
+
+- comparing numbers of people, rats and deaths in different areas of London.
+- testing the equation d = (0.8 x r) x (1.3 x p) by comparing the results to historical data.
+- investigating the impact of different equation weightings.
+
+
+HOW TO RUN THE PROGRAM
+
+- Open blackdeath.ipynb in Jupyter notebooks and run all cells.
+
+
+WHAT TO EXPECT WHEN IT RUNS
+
+When you run the program, a 'Model' window opens. Clicking the 'Model' button will open an options list. 
+Click an option to run a function.
+
+- 'Population graph' : displays a raster map of the human population data in a new window.
+- 'Rat population graph' : displays a raster map of the rat population data in a new window.
+- 'Estimated deaths graph' : a message window requests the user to input custom equation weightings for 
+   rat and human populations, displaying default values of 0.8 and 1.3. When the weightings have been
+   submitted, estimated numbers of deaths are calculated using the weightings and human and rat population
+   data. The deaths data is displayed as a raster map.
+- 'Show total deaths' : a message window requests the user to input custom equation weightings for 
+   rat and human populations, displaying default values of 0.8 and 1.3. When the weightings have been
+   submitted, the total estimated number of deaths is calculated using the weightings and human and rat 
+   population data. The total figure is displayed in a message window.
+- 'Save deaths data as txt' : a message window requests the user to input custom equation weightings for 
+   rat and human populations, displaying default values of 0.8 and 1.3. When the weightings have been
+   submitted, estimated numbers of deaths are calculated using the weightings and human and rat population
+   data. The deaths data is saved as a txt file in the current working directory.
+- 'Exit' : closes the program.
+
+
+LICENCE INFORMATION
